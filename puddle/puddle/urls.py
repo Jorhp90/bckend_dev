7fe4a559bf787300f01458f64ebf9b98d1af258e
Jorge Chavero <jor.hp90@gmail.com>
2023-02-08 19:10:40 -0600

    codewithstein esta demasiado basado

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from core.views import index, contact
@@ -21,4 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name='index'), #empty string cause is the first page, nothing after the first url
     path("contact/", contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
