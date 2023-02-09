a63878cf55dbbbea4a486f399df22c6f9ce6adb9
Jorge Chavero <jor.hp90@gmail.com>
2023-02-09 13:39:52 -0600

    detail page for each item

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -17,11 +17,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name='index'), #empty string cause is the first page, nothing after the first url
     path("contact/", contact, name='contact'),
+    path('items/', include('item.urls'))
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
