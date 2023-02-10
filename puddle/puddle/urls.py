f82e936a95643ad5385c0d53782c06e518c9e9e5
Jorge Chavero <jor.hp90@gmail.com>
2023-02-09 19:00:49 -0600

    til signup, cada dia mas basado

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,12 +18,11 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name='index'), #empty string cause is the first page, nothing after the first url
-    path("contact/", contact, name='contact'),
+    path("", include('core.urls')), 
+    #empty string cause is the first page, nothing after the first url
     path('items/', include('item.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
