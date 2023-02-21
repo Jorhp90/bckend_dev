96589e760f45f19e7bb10e0dadf0ddcdf9766f87
Jorge Chavero <jor.hp90@gmail.com>
2023-02-20 18:11:46 -0600

    adding users restrictions

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name='login_v'),
+    path("logout/", LogoutView.as_view(next_page='tasks_v'), name = 'logout_v'), 
+
     path("", TaskList.as_view(), name='tasks_v'),
     path("task_j/<int:pk>", TaskDetail.as_view(), name='detail_v'),
     path("task-create/", TaskCreate.as_view(), name='task_create_v'),
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -4,6 +4,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.contrib.auth.views import LoginView
 
 from .models import Task
@@ -18,27 +20,43 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks_v')
 
 # Create your views here.
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     """template with query-set updated"""
     model = Task
     context_object_name = 'tasks' #change the context object name for the template
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html' #add the specific template to connect
 
-class TaskCreate(CreateView):
+
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__' #all the fields from the model
+    fields = ['title', 'description', 'complete']
+    #__all__ means all the fields from the model
     success_url = reverse_lazy('tasks_v')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__' #all the fields from the model
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks_v')
 
-class TaskDelete(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks_v')
