3bafde9ad8f31016437f979e4e6ff339da6cb0a7
Jorge Chavero <jor.hp90@gmail.com>
2023-05-04 12:56:45 -0600

    security stuff resolved

--- real_python_social_network/dwitter/views.py
+++ real_python_social_network/dwitter/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from dwitter.models import Profile, Dweet
 
 def dashboard(request):
+    """simple base view"""
     return render(request, template_name="base.html")
 
 def profile_list(request):
+    """show a list of all profiles except for the user"""
     profiles = Profile.objects.exclude(id=request.user.id) #Get all user profiles except for your own
     context = {'profiles':profiles}
     return render(request, template_name="dwitter/profile_list.html", context=context)
 
 def profile(request, pk):
-    profile = Profile.objects.get(pk=pk)
+    """list of followers and followed by
+        follow/unfollow
+        list of dweets; straight from the template
+    """
+    profile = Profile.objects.get(pk=pk) #get the logged user
     if request.method == "POST":
         current_user_profile = request.user.profile
         data = request.POST
