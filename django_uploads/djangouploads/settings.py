3f2b89c866c3c8665b88c4013f461ca90219c5e6
Jorge Chavero <jor.hp90@gmail.com>
2023-05-14 19:25:52 -0600

    fixing secret key

--- django_uploads/djangouploads/settings.py
+++ django_uploads/djangouploads/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_s*iw1kcn^bgq^5lm%p=zyll4ez4wfo^^7h^c3&xjp)hk#h4ie'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
